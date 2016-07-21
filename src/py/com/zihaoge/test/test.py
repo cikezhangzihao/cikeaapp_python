4743bf4212305a101acb33b62a2586e59f53e1f9
cike_qianfan <13274008954@163.com>
2016-07-21 17:02:22 +0800

    提交说明

---
+++ src/py/com/zihaoge/test/test.py
@@ -0,0 +1,26 @@
+from Tools.Scripts.treesync import raw_input
+
+if True:  # 备注代码
+    print("is True")
+else:
+    print("is False")
+
+word = '' \
+       'cike'
+name = "my name is cike "
+remark = """
+        i like rolleco
+"""
+print(word + name + remark)
+# raw_input("\n\nPress the enter key to exit.")
+
+age = 18
+name = "cake"
+print(age)
+print(name)
+
+# 创建多个变量 为多个变量赋值
+a, b, c = "name", 32, 18
+
+print
+name[2:3]
